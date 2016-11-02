dd9ee0e653376123bb9fc2a7fdf0e3b0b67b2a8b
Eduard Kerkhoven <eduardk@chalmers.se>
2016-11-02 21:21:57 +0100

    Add option to plot centroid

--- plotClusters.R
+++ plotClusters.R
@@ -2,27 +2,46 @@
 # Input:
 #           kmeans    Output from kmeans clustering (should contain kmeans$cluster)
 #           normData  Matrix containing normalized data (was used as input for kmeans clustering
+#           centroid  Either 'centroid', 'genes' or 'both'
 # Output:
 #           plots
 ## 2016-10-25 Eduard Kerkhoven
 
-plotClusters<-function(k,normdata){
+plotClusters <- function(k, normdata, centroid) {
   # Determine number of clusters
-  max(k$cluster)
-  out <- data.frame(gene=character(),
-                   variable=character(), 
-                   value=numeric(),
-                   cluster=numeric(),
-                   stringsAsFactors=FALSE) 
-  for (i in 1:max(k$cluster)){ # Loop through all clusters
-    dat<-names(k$cluster[k$cluster==i]) # Genes in cluster
-    dat<-data.frame(normdata[dat,]) # Extract normalized data for genes in cluster
-    dat$gene<-rownames(dat) # Explicitely add gene names
-    dat<-melt(dat,id="gene")
-    dat$cluster<-i
-    out<-rbind(out,dat)
-  }
-  head(out)
-  out<-out
-  ggplot(out,aes(x=variable,y=value,group=gene)) + geom_line() + facet_wrap(~cluster,drop=T)
+    max(k$cluster)
+    out <- data.frame(
+      gene = character(),
+      variable = character(),
+      value = numeric(),
+      cluster = numeric(),
+      stringsAsFactors = FALSE
+    )
+    centrOut <- data.frame(
+      value=numeric(),
+      cluster=numeric(),
+      variable=character(),
+      stringsAsFactors=FALSE
+    )
+    for (i in 1:max(k$cluster)) {
+      # Loop through all clusters
+      dat <- names(k$cluster[k$cluster == i]) # Genes in cluster
+      dat <-
+        data.frame(normdata[dat, ]) # Extract normalized data for genes in cluster
+      cent<-data.frame(value=colMeans(dat,na.rm=T))
+      cent$cluster<-i
+      cent$variable<-rownames(cent)
+      centrOut<-rbind(centrOut,cent)
+      dat$gene <- rownames(dat) # Explicitely add gene names
+      dat <- melt(dat, id = "gene")
+      dat$cluster <- i
+      out <- rbind(out, dat)
+    }
+    head(out)
+    out <- out
+    switch(centroid,
+           both=ggplot(out, aes(x = variable, y = value, group = gene)) + geom_line() + geom_line(data=centrOut, aes(x=variable,y=value,group=1), colour='red',size=1)  + facet_wrap( ~cluster, drop = T),
+           centroid=ggplot() + geom_line(data=centrOut, aes(x=variable,y=value,group=1), colour='red',size=1)  + facet_wrap( ~cluster, drop = T),
+           genes=ggplot(out, aes(x = variable, y = value, group = gene)) + geom_line() + facet_wrap( ~cluster, drop = T)
+    )
 }
