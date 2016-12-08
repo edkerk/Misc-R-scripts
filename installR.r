278f571e6f7171f9b0704f77686b0efdf9b89b0a
Eduard Kerkhoven <eduardk@chalmers.se>
2016-12-08 09:36:28 +0100

    Add bioconductor upgrade

--- installR.r
+++ installR.r
@@ -1,3 +1,4 @@
 source("https://bioconductor.org/biocLite.R")
+biocLite("BiocUpgrade")
 biocLite(c("piano","limma","edgeR","ggplot2","snowfall","plyr","dplyr","tidyr","gdata","affy","plier","DESeq"))
 install.packages(c("labeling","devtools","VennDiagram"))
