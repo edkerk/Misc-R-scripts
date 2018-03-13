737cb96c11187c976fb48caf0b6031e078e73282
Eduard Kerkhoven <eduardk@chalmers.se>
2018-03-13 15:55:03 +0100

    doc: specify usage of this script

--- installR.r
+++ installR.r
@@ -1,3 +1,6 @@
+# This script installs packages that I routinely use.
+# Run this after a clean installation of R.
+# 2018-03-13  Eduard Kerkhoven
 source("https://bioconductor.org/biocLite.R")
 biocLite("BiocUpgrade")
 biocLite(c("piano","limma","edgeR","ggplot2","snowfall","plyr","dplyr","tidyr","gdata","affy","plier","DESeq"))
