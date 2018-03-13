3c00f031de00fbb8f51d952299a96e8da28f3f01
Eduard Kerkhoven <eduardk@chalmers.se>
2018-03-13 15:55:35 +0100

    feat: installR additional packages
    and refactor tidyverse packages

--- installR.r
+++ installR.r
@@ -1,7 +1,8 @@
 # This script installs packages that I routinely use.
 # Run this after a clean installation of R.
 # 2018-03-13  Eduard Kerkhoven
+.libPaths("C:/Work/Rlib")
 source("https://bioconductor.org/biocLite.R")
 biocLite("BiocUpgrade")
-biocLite(c("piano","limma","edgeR","ggplot2","snowfall","plyr","dplyr","tidyr","gdata","affy","plier","DESeq"))
-install.packages(c("labeling","devtools","VennDiagram"))
+biocLite(c("piano","limma","edgeR","snowfall","tidyverse","gdata","affy","plier","DESeq"))
+install.packages(c("labeling","devtools","VennDiagram","cluster","lattice","survival"))
